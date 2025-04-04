8a51bc0f51285fb7ce972fb390185474a765e22c
D.M.S.P. Disanayaka <151613583+sachii4488@users.noreply.github.com>
2025-04-04 14:09:22 +0530

    done1

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
                 <img src={Yash} alt="" />
             </div>
             <div className="about-right">
-                <h2>About Me</h2>
+                <h2>About Us</h2>
                 <p>Hello, we are Yashoda Gunawardhana & Sachini Disanayaka a third-year undergraduate students pursuing a Bachelor of Science in Engineering at the Faculty of Engineering, University of Ruhuna. Our major is in Computer Engineering, where we are deeply immersed in both theoretical and practical aspects of the field</p>
                 <p>Throughout my academic journey, I have developed a keen interest in various domains of computer engineering, including software development, data analysis, and particularly, cybersecurity. I am passionate about leveraging technology to solve real-world problems and continuously seek opportunities to expand my knowledge and skills.</p>
                 <p>In addition to our coursework, I actively engage in extracurricular activities and projects that enhance my understanding and application of engineering principles. we enjoy collaborating with peers on innovative projects and participating in competitions that challenge our problem-solving abilities.Looking ahead, we are excited about the potential to contribute to advancements in technology and to develop solutions that have a meaningful impact on society. We are eager to gain hands-on experience through internships and research opportunities, and to connect with professionals in the field to further our growth and career aspirations.</p>
